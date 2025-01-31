fb7c29b55c9325f779485acd8c04add731a2fdde
Tommy Long <tlong10@myseneca.ca>
2025-01-31 17:26:39 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Tommy Long
+#Author ID: TLONG10
+#Date Created: 2024/01/31
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
