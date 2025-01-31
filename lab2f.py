6e46abca2e5a355c1fabde2e1706ebcb9aa8c6c6
Tommy Long <tlong10@myseneca.ca>
2025-01-31 17:30:58 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Tommy Long
+#Author ID: TLONG10
+#Date Created: 2024/01/31
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
